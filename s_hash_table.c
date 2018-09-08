3b37c3bdfdb0742920cfb802c8c9060864d588d8
Seungha Son <linuxias@gmail.com>
2018-09-08 22:08:57 +0900

    Init SHashTable
    Signed-off-by: Seungha Son <linuxias@gmail.com>

---
+++ s_hash_table.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#include "s_hash_table.h"
+#include "s_macro.h"
+
+SHashTable *s_hash_table_create()
+{
+	SHashTable *table = NULL;
+
+	table = (SHashTable *)calloc(1, sizeof(SHashTable));
+	if S_UNLIKELY (table == NULL)
+		return NULL;
+
+	return table;
+}
+
+void s_hash_table_destroy(SHashTable *table)
+{
+	if (table)
+		free(table);
+}
---
+++ s_hash_table.h
@@ -0,0 +1,11 @@
+#ifndef __S_HASH_TABLE__
+#define __S_HASH_TABLE__
+
+typedef struct {
+
+} SHashTable;
+
+SHashTable *s_hash_table_create();
+
+void s_hash_table_destroy(SHashTable *table);
+#endif /* __S_HASH_TABLE__ */
