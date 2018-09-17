58c1a561d41ce0e61ecf816705b7533d37c571e5
Seungha Son <seungha.son@samsung.com>
2018-09-17 09:36:56 +0900

    Fix macro name for type conversion
    Signed-off-by: Seungha Son <seungha.son@samsung.com>

--- include/s_type_conversion.h
+++ include/s_type_conversion.h
@@ -6,6 +6,6 @@
 #define SINT_TO_POINTER(x) ((void *)(long)(x))
 #define SPOINTER_TO_INT(x) ((int)(long)(x))
 #define SUINT_TO_POINTER(x) ((void *)(unsigned long)(x))
-#define SPOINTER_TO_INT(x) ((unsigned int)(unsigned long)(x))
+#define SPOINTER_TO_UINT(x) ((unsigned int)(unsigned long)(x))
 
 #endif /* __S_TYPE_CONVERSAION_H__*/
